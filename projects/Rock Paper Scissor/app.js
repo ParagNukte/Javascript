924d6eb9aa1e9caf6076a50153e8368e51f38b60
ParagNukte <paragnuktek@gmail.com>
2025-08-04 19:05:05 +0530

    fixed: folderstr branch change

---
+++ projects/Rock Paper Scissor/app.js
@@ -0,0 +1,53 @@
+let compScore = 0;
+let selfScore = 0;
+let msg = document.querySelector("#MoveSelector");
+
+function generateCompMove() {
+  let options = ["rock", "paper", "sissor"];
+  let optIdx = Math.floor(Math.random() * 3);
+  return options[optIdx];
+}
+
+const resultDraw = () => {
+  msg.innerText = "Draw";
+  msg.style.backgroundColor = "grey"; // Reset the background color
+};
+
+function checkWhoWin(selfClickedValue) {
+  let selfMove = selfClickedValue;
+  let whoWin = "";
+  let compMove = generateCompMove();
+
+  if (selfMove === compMove) resultDraw();
+  if (
+    (selfMove === "rock" && compMove === "paper") ||
+    (selfMove === "paper" && compMove === "sissor") ||
+    (selfMove === "sissor" && compMove === "rock")
+  ) {
+    whoWin = "compWin";
+    compScore += 1;
+  }
+  if (
+    (selfMove === "paper" && compMove === "rock") ||
+    (selfMove === "sissor" && compMove === "paper") ||
+    (selfMove === "rock" && compMove === "sissor")
+  ) {
+    whoWin = "selfWin";
+    selfScore += 1;
+  }
+  displayResult(selfMove, compMove, whoWin);
+}
+
+function displayResult(selfMove, compMove, whoWin) {
+  if (whoWin === "selfWin") {
+    msg.innerText = `You won. ${selfMove} beats ${compMove}`;
+    msg.style.backgroundColor = "green"; // Change the background color to green
+  }
+  if (whoWin === "compWin") {
+    msg.innerText = `Comp Won. ${compMove} beats your ${selfMove}`;
+    msg.style.backgroundColor = "red"; // Reset the background color
+  }
+
+  document.querySelector("#selfResult").innerText = `${selfScore}`;
+  document.querySelector("#compResult").innerText = ` ${compScore}`;
+}
